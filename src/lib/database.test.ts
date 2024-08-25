dfc8ac941db77e47ad01daf2de50712aa28c6fb7
Dan Forys <d@forys.co.uk>
2024-08-25 20:12:26 +0100

    WIP

---
+++ src/lib/database.test.ts
@@ -0,0 +1,19 @@
+import { DummySQLConnection } from "../databases/dummy";
+
+import { stringColumn } from "./columns";
+import { Database } from "./database";
+
+describe("The Database class", () => {
+  it("can be instantiated", () => {
+    const dbInstance = new Database(new DummySQLConnection());
+    expect(() =>
+      dbInstance.getMany({
+        columns: { test: stringColumn() },
+        query: (input: string) => ({
+          sql: `DUMMY SQL`,
+          values: [input],
+        }),
+      })
+    ).not.toThrow();
+  });
+});
