90a44e2794231f4e755cc482a2096190ca2c96af
Dan Forys <d@forys.co.uk>
2024-09-26 13:37:13 +0100

    Fix integration tests

--- packages/integration-test/jest.config.js
+++ packages/integration-test/jest.config.js
@@ -1,7 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: "node",
+  // [...]
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
+    "^.+\\.m?[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
 };
